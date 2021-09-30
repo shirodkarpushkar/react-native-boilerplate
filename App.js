99e71ecb62a6e91775ca269270142a1ef0f69283
shirodkarpushkar <shirodkarpushkar.9@gmail.com>
2021-09-30 19:02:56 +0530

    modal view

--- App.js
+++ App.js
@@ -1,112 +1,131 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
 import React from 'react';
-// import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TextInput,
+  Button,
+  FlatList,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+import ListItem from './src/components/ListItem/ListItem';
+import placeImg from './src/assets/sus3.jpg';
+import PlaceDetails from './src/components/PlaceDetails';
+export default class App extends React.Component {
+  state = {
+    placeName: '',
+    places: [],
+    place: null,
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+  onPlaceChange = value => {
+    this.setState({
+      placeName: value,
+    });
+  };
+  onSubmit = e => {
+    console.log('🚀 ~ file: App.js ~ onSubmit ~ e', e);
+    if (this.state.placeName.trim()) {
+      this.setState({
+        places: [
+          ...this.state.places,
+          {
+            key: Date.now().toString(),
+            value: this.state.placeName,
+            src: placeImg,
+          },
+        ],
+      });
+      console.log('places==>', this.state.places);
+    }
+  };
+  onListItemPressed = (place, i) => {
+    // alert(`Item pressed #${i} : ${JSON.stringify(place)}`);
+    this.setState({place});
+  };
+  onDelete = e => {
+    const places = this.state.places;
+    const filteredPlaces = places.filter(el => el.key !== this.state.place.key);
+    this.setState({
+      places: [...filteredPlaces],
+      place: null,
+    });
+  };
+  onClose = e => {
+    this.setState({
+      place: null,
+    });
+  };
+
+  render() {
+    const {places} = this.state;
+    const placesList = (
+      <FlatList
+        style={styles.listContainer}
+        data={places}
+        renderItem={({item, index}) => (
+          <ListItem
+            src={item.src}
+            placeName={item.value}
+            onPress={() => this.onListItemPressed(item, index)}
+          />
+        )}
+      />
+    );
+
+    return (
+      <View style={styles.container}>
+        <PlaceDetails
+          place={this.state.place}
+          onDelete={this.onDelete}
+          onClose={this.onClose}
+        />
+        <Text>Input here</Text>
+        <View style={styles.formgroup}>
+          <TextInput
+            style={styles.forminput}
+            value={this.state.placeName}
+            onChangeText={this.onPlaceChange}
+            placeholder="awesome place"
+          />
+          <Button
+            style={styles.button}
+            title=" + Add "
+            onPress={this.onSubmit}
+          />
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+        <Text>Your Places</Text>
+        {placesList}
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  forminput: {
+    flex: 1,
+    width: 300,
+    borderBottomWidth: 1,
+    height: 35,
+    borderBottomColor: '#eee',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+
+  formgroup: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    flex: 1,
+  },
+  listContainer: {
+    width: '100%',
   },
 });
-
-export default App;
