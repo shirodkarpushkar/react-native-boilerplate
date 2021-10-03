7f4456dffb11f9143848d8410015c7e730a421c4
shirodkarpushkar <shirodkarpushkar.9@gmail.com>
2021-10-03 18:05:00 +0530

    router navigate

--- src/screens/CategoriesScreen.js
+++ src/screens/CategoriesScreen.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
 const CategoriesScreen = props => {
+  
   return (
     <View style={styles.screen}>
       <Text>The Categories Screen</Text>
+      <Button
+        title="Go to Category Meals"
+        onPress={() => props.navigation.navigate('CategoryMeals')}
+      />
     </View>
   );
 };
+
+const routeTo = props => {
+  props.navigation.navigate('CategoryMeals');
+};
+
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
