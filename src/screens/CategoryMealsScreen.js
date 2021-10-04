de8e511817bc601d79c1899f391d67131e79e79a
shirodkarpushkar <shirodkarpushkar.9@gmail.com>
2021-10-04 12:01:35 +0530

    meals styling

--- src/screens/CategoryMealsScreen.js
+++ src/screens/CategoryMealsScreen.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-import {CATEGORIES} from '../data/dummy-data';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
+import {CATEGORIES, MEALS} from '../data/dummy-data';
 const CategoriesMealsScreen = props => {
   const {categoryId} = props.route.params;
   const category = CATEGORIES.find(el => el.id === categoryId);
   props.navigation.setOptions({title: category.title});
+  const meals = MEALS.filter(el => el.categoryIds.indexOf(categoryId) >= 0);
+  const renderMealItem = ({item}) => {
+    return (
+      <TouchableOpacity onPress={() => {}} style={styles.mealItem}>
+        <View>
+          <View style={{...styles.mealRow, ...styles.mealHeader}}>
+            <ImageBackground
+              source={{uri: item.imageURL}}
+              style={styles.bgImage}>
+              <Text style={styles.title} numberOfLines={1}>
+                {item.title}
+              </Text>
+            </ImageBackground>
+          </View>
+          <View style={{...styles.mealRow, ...styles.mealDetails}}>
+            <Text>{item.duration}m</Text>
+            <Text>{item.complexity.toUpperCase()}</Text>
+            <Text>{item.affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.screen}>
-      <Text>The Categories Meals Screen</Text>
-      <Text>{category.title}</Text>
-      <Button title="Go to Details !" onPress={() => routeTo(props)} />
-      <Text>{'\n'}</Text>
-      <Button title="Go Back !" onPress={() => props.navigation.goBack()} />
+      <FlatList
+        data={meals}
+        renderItem={renderMealItem}
+        style={{width: styles.mealItem.width}}
+      />
     </View>
   );
 };
@@ -24,6 +56,42 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 10,
+  },
+  mealRow: {
+    flexDirection: 'row',
+  },
+  mealItem: {
+    height: 200,
+    width: '100%',
+    backgroundColor: '#e5e5e5',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginBottom: 10,
+  },
+  mealHeader: {
+    height: '85%',
+  },
+  mealDetails: {
+    paddingHorizontal: 10,
+    height: '15%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  mealContainer: {},
+  bgImage: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end',
+  },
+  title: {
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#fff',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    paddingVertical: 5,
+    paddingHorizontal: 12,
   },
 });
 export default CategoriesMealsScreen;
