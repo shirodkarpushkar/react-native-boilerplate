f836d627ea4e123149cf851f08ed7d9b702050b8
shirodkarpushkar <shirodkarpushkar.9@gmail.com>
2021-10-04 13:21:08 +0530

    Tab navigator

--- src/navigation/MealsNavigator.js
+++ src/navigation/MealsNavigator.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailsScreen from '../screens/MealDetailsScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
-import { HeaderButtons } from 'react-navigation-header-buttons';
+import {HeaderButtons} from 'react-navigation-header-buttons';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 // const MealsNavigator =  createStackNavigator({
 //   Categories: CategoriesScreen,
@@ -14,26 +17,64 @@ import { HeaderButtons } from 'react-navigation-header-buttons';
 // });
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const MealsNavigator = () => {
   return (
+    <Stack.Navigator initialRouteName="Categories">
+      <Stack.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: 'Meal Categories',
+          headerStyle: {
+            backgroundColor: Colors.primaryColor,
+          },
+          headerTintColor: '#fff',
+        }}
+      />
+      <Stack.Screen name="CategoryMeals" component={CategoryMealsScreen} />
+      <Stack.Screen name="MealDetails" component={MealDetailsScreen} />
+    </Stack.Navigator>
+  );
+};
+const MealsTabNavigator = () => {
+  return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Categories">
-        <Stack.Screen
-          name="Categories"
-          component={CategoriesScreen}
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: Colors.accentColor,
+        }}>
+        <Tab.Screen
+          name="Meals"
+          component={MealsNavigator}
           options={{
-            title: 'Meal Categories',
-            headerStyle: {
-              backgroundColor: Colors.primaryColor,
+            
+            tabBarIcon: tabInfo => {
+              return (
+                <Icon name="ios-restaurant" size={25} color={tabInfo.color} />
+              );
             },
-            headerTintColor: '#fff',
           }}
         />
-        <Stack.Screen name="CategoryMeals" component={CategoryMealsScreen} />
-        <Stack.Screen name="MealDetails" component={MealDetailsScreen} />
-      </Stack.Navigator>
+        <Tab.Screen
+          name="Favorites"
+          component={FavoritesScreen}
+          options={{
+            title:"Favorites !",
+            tabBarIcon: tabInfo => {
+              return (
+                <Icon
+                  name="ios-star"
+                  size={25}
+                  color={tabInfo.color}
+                />
+              );
+            },
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
-export default MealsNavigator;
+export default MealsTabNavigator;
